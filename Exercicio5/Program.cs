c325df1904cb6989d2c4a34e7a677f1ec27280a6
miguelmilagres <miguelmdemacedo@gmail.com>
2024-10-31 18:08:29 -0300

    exercicio5

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio5;
+
+class Program
+{
+    static void Main()
+    {
+        bool result = false;
+
+        Console.Write("Digite a placa do seu veículo: ");
+        string? placa = Console.ReadLine();
+
+        if (placa is not null)
+            if (placa.All(Char.IsLetterOrDigit) && placa.Length == 7)
+                if (placa.Substring(0, 3).All(Char.IsLetter))
+                    if (placa.Substring(3).All(Char.IsNumber))
+                        result = true;
+
+        if (result)
+            Console.WriteLine("Verdadeiro");
+        else
+            Console.WriteLine("Falso");
+    }
+}
