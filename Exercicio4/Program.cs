3b89837d344137ee57911b09ef336ad001e5790c
miguelmilagres <miguelmdemacedo@gmail.com>
2024-10-31 17:52:21 -0300

    exercicio4

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio4;
+
+class Program
+{
+    static void Main()
+    {
+        string texto = Console.ReadLine();
+        string textoSemEspacos = texto.Trim();
+
+        int contador = 0;
+        for (int i = 0, n = textoSemEspacos.Length; i < n; i++)
+        {
+            if (textoSemEspacos[i] != ' ')
+                contador++;
+        }
+
+        Console.WriteLine($"Quantidade de caracteres: {contador}");
+    }
+}
