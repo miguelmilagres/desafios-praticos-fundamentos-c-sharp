05efe223bdde2e78f9506634e85473f03985ca63
miguelmilagres <miguelmdemacedo@gmail.com>
2024-10-31 17:33:22 -0300

    exercicio1

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercicio1;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine($"Olá, {nome}! Seja muito bem-vindo!");
+    }
+}
